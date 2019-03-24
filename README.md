7dacb2bd45a06db6274e11a5c6a9d4b4a4145163
Shijie Zhou <cs5129606@gmail.com>
2019-03-23 23:17:14 -0600

    BEST VERSION

--- README.md
+++ README.md
@@ -2,18 +2,8 @@
 
 ![](https://img.shields.io/github/stars/pandao/editor.md.svg) ![](https://img.shields.io/github/forks/pandao/editor.md.svg) ![](https://img.shields.io/github/tag/pandao/editor.md.svg) ![](https://img.shields.io/github/release/pandao/editor.md.svg) ![](https://img.shields.io/github/issues/pandao/editor.md.svg) ![](https://img.shields.io/bower/v/editor.md.svg)
 
-# vue-live
+# vue-portfolio
 
--+++++=        .+++++=
-.+JJJJJ+       #JJJJ*:
-  .JJJJJ=     *JJJJJ  
-   J+JJJJ-   =#JJJJJ  
-   J +JJJJ: :% JJJJJ  
-   J  *JJJJ-%: JJJJJ  
-   J   *JJJJ-  JJJJJ  
-  -J-   #JJ+  :JJJJJ: 
--#JJJ#-  ##  =JJJJJJJ=
-.......      .........
 
 ## Project setup
 ```
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,42 +1,47 @@
 <template>
   <div class="about">
-    <div class="logo-wrap">
-      <a href="https://www.shijiezhou.com"><img alt="SZ logo" class="szlogo" src="../assets/logo_j.svg"></a>
-    </div>
-    
-    <section id="footer">
-      <div class="container">
-        <div class="">
-          <p>
-            Copyright ©
-            <a href="https://www.shijiezhou.com">Shijie Zhou</a>
-          </p>
-        </div>
-        <!-- // .desktop-3 -->
-        <div class="clear"></div>
+    <b-container>
+      <div class="logo-wrap mb-3">
+        <a href="https://www.shijiezhou.com"><img
+            alt="SZ logo"
+            class="szlogo"
+            src="../assets/logo_j.svg"
+          ></a>
       </div>
-      <!-- // .container -->
+      <div class="about-text">Shijie Zhou studies at University of California, San Diego in the Interdisciplinary Computer in Arts. He is also the member for design and construct web developer at the Worldview project organization.
+        He was working on programming artist and digital image designer at the Digital Gym Cinema. He obtains Associate degree in Green River College, and also finish the B.A. degree at UCSD.
+        He gained experience in media use and computer art. He also familiar with web design and selling ebay stuffs at leisure time..
+      </div>
+    </b-container>
+    <section id="footer mt-3">
+      <div class="">
+        © Copyright Agency and contributors 2013 - {{new Date().getFullYear()}} - All Rights Reserved by -
+        <a href="https://www.shijiezhou.com">Shijie Zhou</a>
+      </div>
+      <div class="clear"></div>
     </section>
   </div>
 </template>
 
 <script>
 export default {
-  
+
 }
 </script>
 
 <style scoped>
-.about{
+.about {
   height: 800px;
+}
+.about-text {
+  text-align: center;
 }
 .szlogo {
   width: 100px;
   height: 95px;
   border: 2px solid lightskyblue;
 }
-
-.szlogo:hover{
-    -webkit-filter: invert(60%) !important;
+.szlogo:hover {
+  -webkit-filter: invert(60%) !important;
 }
 </style>
