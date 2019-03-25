99bc4c05bb03fa2249817dd1505eac61d90995c8
Shijie Zhou <cs5129606@gmail.com>
2019-03-24 18:58:50 -0600

    UPDATE REAL SOCIAL MEDIA

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -1,10 +1,10 @@
 <template>
     <b-row class="mt-2">
       <b-container>
-        <a class="home_icon" href="https://github.com/shijiezhou1/"><font-awesome-icon :icon="['fab', 'github']" /></a>
-        <a class="home_icon" href="https://github.com/shijiezhou1/"><font-awesome-icon :icon="['fab', 'facebook']" /></a>
-        <a class="home_icon" href="https://github.com/shijiezhou1/"><font-awesome-icon :icon="['fab', 'medium']" /></a>
-        <a class="home_icon" href="https://github.com/shijiezhou1/"><font-awesome-icon :icon="['fab', 'twitter']" /></a>
+        <a class="home_icon" href="https://github.com/shijiezhou1/" target="_blank"><font-awesome-icon :icon="['fab', 'github']" /></a>
+        <a class="home_icon" href="https://www.facebook.com/profile.php?id=100003975309210" target="_blank"><font-awesome-icon :icon="['fab', 'facebook']" /></a>
+        <a class="home_icon" href="https://medium.com/@cs5129606" target="_blank"><font-awesome-icon :icon="['fab', 'medium']" /></a>
+        <a class="home_icon" href="https://shijiezhou.tumblr.com/" target="_blank"><font-awesome-icon :icon="['fab', 'tumblr-square']" /></a>
       </b-container>
     </b-row>
 </template>
