0862b9b4d288da85820f863d2749d2b06f5822db
Shijie Zhou <cs5129606@gmail.com>
2019-04-03 19:22:04 -0600

    have a try

---
+++ scripts/deploy-to-gh-pages.sh
@@ -0,0 +1,18 @@
+set -o errexit
+
+rm -rf public
+mkdir public
+
+# config
+git config --global user.email "cs5129606@gmail.com"
+git config --global user.name "Jay"
+
+# build (CHANGE THIS)
+npm run build
+
+# deploy
+cd public
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/vue-portfolio.git" master:gh-pages > /dev/null 2>&1
