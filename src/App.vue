f5a8ffdf6fd64721e273928f80bc2d91add8baba
Shijie Zhou <cs5129606@gmail.com>
2019-04-09 22:31:20 -0600

    teest

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,16 @@ import Nav from '@/components/Nav.vue'
 import NotificationContainer from '@/components/NotificationContainer.vue'
 
 export default {
-   components: {
-     Nav,
-     NotificationContainer
-   }
+  components: {
+    Nav,
+    NotificationContainer
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
